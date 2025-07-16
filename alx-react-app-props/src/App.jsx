6e6a4c36c06b9619e15a933f55fe1f355b3a05b0
akarani-cs <alvinmudembei@gmail.com>
2025-07-16 13:24:39 +0300

    'newer'

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,23 @@
+import WelcomeMessage from './WelcomeMessage.jsx'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+import Profilepage from './ProfilePage.jsx'
+import UserContext from './UserContext.jsx'
+
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+
+export default App
