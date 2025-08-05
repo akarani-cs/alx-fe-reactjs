c9b08123b60b216fbd7bb951e502505ce2788a78
akarani-cs <alvinmudembei@gmail.com>
2025-08-05 12:56:57 +0300

    additional styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,9 +4,9 @@ function UserProfile() {
 
 
 
-        <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg "> 
-        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto"/>
-        <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+        <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl "> 
+        <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto md:w-36 md:h-36 object-cover"/>
+        <h1 className="text-xl text-blue-800 my-4 md:max-w-sm mx-auto bg-white shadow rounded-lg">John Doe</h1>
         <p className="text-gray-600 text-base">Software Engineer</p>
          </div>
     )
