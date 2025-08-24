593d0a99d2c986042cdf94df8bf0b5c9474aa5a6
AlvinKarani <alvinmudembei@gmail.com>
2025-08-24 18:17:14 +0300

    new

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Welcome to my React application!</p>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,3 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
+
+
+//"PostsComponent"
